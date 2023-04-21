de63e8e40585d9ebf0af2e6f0ee18219788c8846
Mortyje <mortyje@gmail.com>
2023-04-21 22:06:14 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace course_work_in_programming_1_course
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBoxHigh = new System.Windows.Forms.CheckBox();
+            this.checkBoxMedium = new System.Windows.Forms.CheckBox();
+            this.checkBoxLow = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(59)))), ((int)(((byte)(64)))));
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listView1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.ForeColor = System.Drawing.SystemColors.Window;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(878, 540);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TASK";
+            this.columnHeader1.Width = 490;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "DATE";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "TIME";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 65;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "EXECUTOR";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "PRIORITY";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 100;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(32)))), ((int)(((byte)(35)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(970, 136);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(150, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD TASK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(32)))), ((int)(((byte)(35)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Window;
+            this.button2.Location = new System.Drawing.Point(970, 287);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(150, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DELETE TASK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(32)))), ((int)(((byte)(35)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Window;
+            this.button3.Location = new System.Drawing.Point(970, 450);
+            this.button3.Name = "button3";
+            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button3.Size = new System.Drawing.Size(150, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
+            this.button3.MouseLeave += new System.EventHandler(this.button3_MouseLeave);
+            // 
+            // checkBoxHigh
+            // 
+            this.checkBoxHigh.AutoSize = true;
+            this.checkBoxHigh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBoxHigh.ForeColor = System.Drawing.Color.White;
+            this.checkBoxHigh.Location = new System.Drawing.Point(901, 12);
+            this.checkBoxHigh.Name = "checkBoxHigh";
+            this.checkBoxHigh.Size = new System.Drawing.Size(126, 17);
+            this.checkBoxHigh.TabIndex = 4;
+            this.checkBoxHigh.Text = "Высокий приоритет";
+            this.checkBoxHigh.UseVisualStyleBackColor = true;
+            this.checkBoxHigh.CheckedChanged += new System.EventHandler(this.checkBoxHigh_CheckedChanged);
+            // 
+            // checkBoxMedium
+            // 
+            this.checkBoxMedium.AutoSize = true;
+            this.checkBoxMedium.ForeColor = System.Drawing.Color.White;
+            this.checkBoxMedium.Location = new System.Drawing.Point(901, 35);
+            this.checkBoxMedium.Name = "checkBoxMedium";
+            this.checkBoxMedium.Size = new System.Drawing.Size(124, 17);
+            this.checkBoxMedium.TabIndex = 5;
+            this.checkBoxMedium.Text = "Средний приоритет";
+            this.checkBoxMedium.UseVisualStyleBackColor = true;
+            this.checkBoxMedium.CheckedChanged += new System.EventHandler(this.checkBoxMedium_CheckedChanged);
+            // 
+            // checkBoxLow
+            // 
+            this.checkBoxLow.AutoSize = true;
+            this.checkBoxLow.ForeColor = System.Drawing.Color.White;
+            this.checkBoxLow.Location = new System.Drawing.Point(901, 58);
+            this.checkBoxLow.Name = "checkBoxLow";
+            this.checkBoxLow.Size = new System.Drawing.Size(119, 17);
+            this.checkBoxLow.TabIndex = 6;
+            this.checkBoxLow.Text = "Низкий приоритет";
+            this.checkBoxLow.UseVisualStyleBackColor = true;
+            this.checkBoxLow.CheckedChanged += new System.EventHandler(this.checkBoxLow_CheckedChanged);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(901, 390);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(32)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(1200, 540);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBoxLow);
+            this.Controls.Add(this.checkBoxMedium);
+            this.Controls.Add(this.checkBoxHigh);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ToDo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("tasks.txt"))
+            {
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    writer.WriteLine($"{item.SubItems[0].Text}\t{item.SubItems[1].Text}\t{item.SubItems[2].Text}\t{item.SubItems[3].Text}\t{item.SubItems[4].Text}");
+                }
+                writer.Flush();
+            }
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private Button button3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private CheckBox checkBoxHigh;
+        private CheckBox checkBoxMedium;
+        private CheckBox checkBoxLow;
+        private Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+
+namespace course_work_in_programming_1_course
+{
+    public partial class Form1 : Form
+    {
+        private bool Drag;
+        private int MouseX;
+        private int MouseY;
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        private bool m_aeroEnabled;
+
+        private const int CS_DROPSHADOW = 0x00020000;
+        private const int WM_NCPAINT = 0x0085;
+        private const int WM_ACTIVATEAPP = 0x001C;
+
+        [System.Runtime.InteropServices.DllImport("dwmapi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+        [System.Runtime.InteropServices.DllImport("dwmapi.dll")]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+        [System.Runtime.InteropServices.DllImport("dwmapi.dll")]
+
+        public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+        [System.Runtime.InteropServices.DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
+
+        public struct MARGINS
+        {
+            public int leftWidth;
+            public int rightWidth;
+            public int topHeight;
+            public int bottomHeight;
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                m_aeroEnabled = CheckAeroEnabled();
+                CreateParams cp = base.CreateParams;
+                if (!m_aeroEnabled)
+                    cp.ClassStyle |= CS_DROPSHADOW; return cp;
+            }
+        }
+        private bool CheckAeroEnabled()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                int enabled = 0; DwmIsCompositionEnabled(ref enabled);
+                return (enabled == 1) ? true : false;
+            }
+            return false;
+        }
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCPAINT:
+                    if (m_aeroEnabled)
+                    {
+                        var v = 2;
+                        DwmSetWindowAttribute(this.Handle, 2, ref v, 4);
+                        MARGINS margins = new MARGINS()
+                        {
+                            bottomHeight = 1,
+                            leftWidth = 0,
+                            rightWidth = 0,
+                            topHeight = 0
+                        }; DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+                    }
+                    break;
+                default: break;
+            }
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT) m.Result = (IntPtr)HTCAPTION;
+        }
+        private void PanelMove_MouseDown(object sender, MouseEventArgs e)
+        {
+            Drag = true;
+            MouseX = Cursor.Position.X - this.Left;
+            MouseY = Cursor.Position.Y - this.Top;
+        }
+        private void PanelMove_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Drag)
+            {
+                this.Top = Cursor.Position.Y - MouseY;
+                this.Left = Cursor.Position.X - MouseX;
+            }
+        }
+        private void PanelMove_MouseUp(object sender, MouseEventArgs e) { Drag = false; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var form = new Form())
+            {
+                form.StartPosition = FormStartPosition.CenterScreen;
+                form.Size = new Size(250, 230);
+
+                var textBox = new TextBox();
+                textBox.Location = new Point(10, 10);
+                textBox.Size = new Size(200, 20);
+                form.Controls.Add(textBox);
+
+                var datePicker = new DateTimePicker();
+                datePicker.Format = DateTimePickerFormat.Short;
+                datePicker.Location = new Point(10, 40);
+                form.Controls.Add(datePicker);
+
+                var timePicker = new DateTimePicker();
+                timePicker.Format = DateTimePickerFormat.Custom;
+                timePicker.CustomFormat = "hh:mm";
+                timePicker.ShowUpDown = true;
+                timePicker.Location = new Point(10, 70);
+                form.Controls.Add(timePicker);
+
+                var TextBoxExecutor = new TextBox();
+                TextBoxExecutor.Location = new Point(10, 100);
+                TextBoxExecutor.Size = new Size(200, 20);
+                form.Controls.Add(TextBoxExecutor);
+
+                var comboBoxPriority = new ComboBox();
+                comboBoxPriority.DropDownStyle = ComboBoxStyle.DropDownList;
+                comboBoxPriority.Location = new Point(10, 130);
+                comboBoxPriority.Size = new Size(200, 20);
+                comboBoxPriority.Items.AddRange(new object[] { "Высокий", "Средний", "Низкий" });
+                form.Controls.Add(comboBoxPriority);
+
+                var buttonOk = new Button();
+                buttonOk.DialogResult = DialogResult.OK;
+                buttonOk.Text = "OK";
+                buttonOk.Location = new Point(10, 160);
+                form.Controls.Add(buttonOk);
+
+                var buttonCancel = new Button();
+                buttonCancel.DialogResult = DialogResult.Cancel;
+                buttonCancel.Text = "Cancel";
+                buttonCancel.Location = new Point(90, 160);
+                form.Controls.Add(buttonCancel);
+
+                form.AcceptButton = buttonOk;
+                form.CancelButton = buttonCancel;
+
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    string[] row = { textBox.Text, datePicker.Value.ToShortDateString(), timePicker.Value.ToShortTimeString(), TextBoxExecutor.Text, comboBoxPriority.SelectedItem.ToString() };
+                    var item = new ListViewItem(row);
+                    listView1.Items.Add(item);
+                }
+
+                using (StreamWriter writer = new StreamWriter("tasks.txt"))
+                {
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        writer.WriteLine($"{item.SubItems[0].Text}\t{item.SubItems[1].Text}\t{item.SubItems[2].Text}\t{item.SubItems[3].Text}\t{item.SubItems[4].Text}");
+                    }
+                    writer.Flush();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // Получаем выбранный элемент ListView
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+
+                // Удаляем выбранный элемент
+                listView1.Items.Remove(selectedItem);
+
+                MessageBox.Show("Задача «" + selectedItem.SubItems[0].Text + "» успешно удалена!");
+            }
+            else
+            {
+                MessageBox.Show("Не выбрана ни одна задача для удаления.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("tasks.txt"))
+            {
+                File.Create("tasks.txt").Close();
+            }
+
+            using (StreamReader reader = new StreamReader("tasks.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] items = line.Split('\t');
+                    var item = new ListViewItem(items);
+                    listView1.Items.Add(item);
+                }
+                reader.Close();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button4_Click(sender, e);
+
+            using (StreamWriter writer = new StreamWriter("tasks.txt"))
+            {
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    writer.WriteLine($"{item.SubItems[0].Text}\t{item.SubItems[1].Text}\t{item.SubItems[2].Text}\t{item.SubItems[3].Text}\t{item.SubItems[4].Text}");
+                }
+                writer.Flush();
+            }
+            this.Close();
+        }
+
+        async void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.FromArgb(31, 32, 35);
+            button3.BackColor = Color.FromArgb(31, 32, 35);
+            for (byte r = 31, g = 32, b = 35; r <= 173 & b <= 216 & g <= 230; r += 7, g += 9, b += 10, await Task.Delay(30))
+            {
+                button1.BackColor = Color.FromArgb(r, g, b);
+            }
+        }
+
+        async void button2_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.FromArgb(31, 32, 35);
+            button3.BackColor = Color.FromArgb(31, 32, 35);
+            for (byte r = 31, g = 32, b = 35; r <= 173 & b <= 216 & g <= 230; r += 7, g += 9, b += 10, await Task.Delay(30))
+            {
+                button2.BackColor = Color.FromArgb(r, g, b);
+            }
+        }
+
+        async void button3_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.FromArgb(31, 32, 35);
+            button2.BackColor = Color.FromArgb(31, 32, 35);
+            for (byte r = 31, g = 32, b = 35; r <= 173 & b <= 216 & g <= 230; r += 7, g += 9, b += 10, await Task.Delay(30))
+            {
+                button3.BackColor = Color.FromArgb(r, g, b);
+            }
+        }
+
+        void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.FromArgb(31, 32, 35);
+        }
+
+        void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.FromArgb(31, 32, 35);
+        }
+
+        void button3_MouseLeave(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.FromArgb(31, 32, 35);
+        }
+
+        private void checkBoxHigh_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateListView();
+
+            if (!checkBoxHigh.Checked)
+            {
+                ShowAllTasks();
+                checkBoxHigh.Checked = false;
+                checkBoxMedium.Checked = false;
+                checkBoxLow.Checked = false;
+            }
+        }
+
+        private void checkBoxMedium_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateListView();
+
+            if (!checkBoxMedium.Checked)
+            {
+                ShowAllTasks();
+                checkBoxHigh.Checked = false;
+                checkBoxMedium.Checked = false;
+                checkBoxLow.Checked = false;
+            }
+        }
+
+        private void checkBoxLow_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateListView();
+
+            if (!checkBoxLow.Checked)
+            {
+                ShowAllTasks();
+                checkBoxHigh.Checked = false;
+                checkBoxMedium.Checked = false;
+                checkBoxLow.Checked = false;
+            }
+        }
+
+        private void UpdateListView()
+        {
+            listView1.Items.Clear();
+            List<string[]> linesToShow = new List<string[]>();
+
+            using (StreamReader reader = new StreamReader("tasks.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] items = line.Split('\t');
+                    if (checkBoxHigh.Checked && line.Contains("Высокий"))
+                    {
+                        linesToShow.Add(items);
+                    }
+                    else if (checkBoxMedium.Checked && line.Contains("Средний"))
+                    {
+                        linesToShow.Add(items);
+                    }
+                    else if (checkBoxLow.Checked && line.Contains("Низкий"))
+                    {
+                        linesToShow.Add(items);
+                    }
+                }
+            }
+
+            foreach (string[] items in linesToShow)
+            {
+                var item = new ListViewItem(items);
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void ShowAllTasks()
+        {
+            listView1.Items.Clear();
+
+            using (StreamReader reader = new StreamReader("tasks.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] items = line.Split('\t');
+                    var item = new ListViewItem(items);
+                    listView1.Items.Add(item);
+                }
+                reader.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            checkBoxHigh.Checked = true;
+            checkBoxMedium.Checked = true;
+            checkBoxLow.Checked = true;
+        }
+    }
+}
